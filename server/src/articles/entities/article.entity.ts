92d967ca19e0b19e6a3a0be25110163ecf1db532
Michael Kudryavets <fathermkv@gmail.com>
2022-04-06 15:41:41 +0300

    naming fix

--- server/src/articles/entities/article.entity.ts
+++ server/src/articles/entities/article.entity.ts
@@ -1,7 +1,7 @@
 import { ApiProperty, PickType } from '@nestjs/swagger';
 import { Article, ArticleStatus } from '@prisma/client';
 import { CreatedUpdatedEntity } from 'src/common/entities';
-import { ArticleLikeEntity } from './article-like-entity';
+import { ArticleLikeEntity } from './article-like.entity';
 import { ArticleTagEntity } from './article-tag.entity';
 
 export class ArticleEntity extends CreatedUpdatedEntity implements Article {
